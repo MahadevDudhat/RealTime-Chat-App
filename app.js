dfcea58f50eb6581d381fd3e90ecfe28643e2c35
MahadevDudhat <mahadevdudhat2003@gmail.com>
2024-08-05 22:51:08 +0530

    first stage of app

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+var mongoose = require("mongoose");
+
+mongoose.connect('mongodb://127.0.0.1:27017/realchat');
+
+const app = require("express")();
+const http = require("http").Server(app);
+
+const userRoute = require("./routes/userRoute");
+app.use('/',userRoute);
+
+http.listen(3000,function(){
+    console.log("server started...")
+});
---
+++ middlewares/auth.js
@@ -0,0 +1,34 @@
+const isLogin = async(req,res,next)=>{
+    try {
+
+        if(req.session.user){
+            next();
+        }
+        else{
+            res.redirect('/');
+        }
+        
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const isLogout = async(req,res,next)=>{
+    try {
+
+        if(req.session.user){
+            res.redirect('/dashboard');
+        }
+       
+        next();
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+module.exports  = {
+    isLogin,
+    isLogout
+};
