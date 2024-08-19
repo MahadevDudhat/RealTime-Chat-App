c926da608f99ed567fc22cfb8e4d5b9bc48c1761
MahadevDudhat <mahadevdudhat2003@gmail.com>
2024-08-19 22:58:17 +0530

    ->implemented chatting functionality. ->fetched and loaded messages from the database. ->fixed the scroll issue

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const http = require("http").Server(app);
 
 const userRoute = require("./routes/userRoute");
 const User = require("./models/userModel");
+const Chat = require("./models/chatModel");
 
 
 app.use('/',userRoute);
@@ -19,20 +20,45 @@ var usp = io.of("/user-namespace");
 
 usp.on("connection",async function(socket){
 
-    console.log("user connected");
-    let userId = socket.handshake.auth.token;
-    await User.findByIdAndUpdate({_id:userId},{$set:{is_online: '1'}});
+    console.log('user connected');
 
-    socket.broadcast.emit("getOnlineUser",{ user_id:userId });
+  let userId = socket.handshake.auth.token;
 
-    socket.on("disconnect",async function(){
-        console.log("user disconnected");
-        let userId = socket.handshake.auth.token;
-        await User.findByIdAndUpdate({_id:userId},{$set:{is_online: '0'}});
-        socket.broadcast.emit("getOfflineUser",{ user_id:userId });
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: '1' } });
+    socket.broadcast.emit('getOnlineUser', { user_id: userId });
+  } catch (error) {
+    console.error('Error updating user online status:', error);
+  }
 
+  socket.on('disconnect', async function () {
+    console.log('user disconnected');
+
+    try {
+      await User.findByIdAndUpdate({ _id: userId }, { $set: { is_online: '0' } });
+      socket.broadcast.emit('getOfflineUser', { user_id: userId });
+    } catch (error) {
+      console.error('Error updating user offline status:', error);
+    }
+  });
+    //chating implementation
+    socket.on("newChat",function(data){
+        socket.broadcast.emit("loadNewChat",data);
     });
+
+    //loading old chats by fetching from database by using find()and using $or
+    socket.on("existChat",async function(data){
+        var chats = await Chat.find({$or:[
+            {sender_id:data.sender_id,receiver_id:data.receiver_id},
+            {sender_id:data.receiver_id,receiver_id:data.sender_id}
+        ]});
+        socket.emit("loadChats",{chats:chats});
+    });
+
+
 });
+
+
 
 
 
